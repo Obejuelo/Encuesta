ae7fb4a07ffd2e42f83732025f7862d649b3535e
Obed Hernandez <obejuela4@gmail.com>
2018-11-03 13:46:50 -0600

    prueba de funcionamiento git

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const morgan = require('morgan');
 const path = require('path');
 const app = express();
 
-//connect to db
+// connect to db
 mongoose.set('useCreateIndex', true);
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true }, (err, res) => {
 	if(err) throw Error(err)
 	// console.log('DB online');
 });
 
-//middlewares
+// middlewares
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(fileUpload());
@@ -21,10 +21,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./middlewares/allowCORs')().unless({ path: '/public' }));
 
-//routes
+// routes
 app.use(require('./routes/index'));
 
-//server listen
+// server listen
 app.listen(process.env.PORT, (err) => {
 	if(err) console.log(err);
 	console.log(`app listen on port ${process.env.PORT}`);
